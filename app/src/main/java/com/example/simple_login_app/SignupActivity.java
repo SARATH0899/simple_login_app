16f5b63f778ee1e345f42f54743ba6ffbe6c0cb6
SARATH0899 <sarathmr16@gmail.com>
2023-12-21 14:11:14 +0530

    Added Validation to SignupActivity

--- app/src/main/java/com/example/simple_login_app/SignupActivity.java
+++ app/src/main/java/com/example/simple_login_app/SignupActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,12 +16,11 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
 
-    EditText signupName, signupEmail, signupMobile, signupUsername,signupPassword, signupConfirmPassword;
+    EditText signupName, signupEmail, signupMobile, signupUsername, signupPassword, signupConfirmPassword;
     TextView loginRedirectText;
     Button signupButton;
     FirebaseDatabase database;
     DatabaseReference reference;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,20 +39,15 @@ public class SignupActivity extends AppCompatActivity {
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = signupName.getText().toString().trim();
+                String email = signupEmail.getText().toString().trim();
+                String mobile = signupMobile.getText().toString().trim();
+                String username = signupUsername.getText().toString().trim();
+                String password = signupPassword.getText().toString().trim();
+                String confirmPassword = signupConfirmPassword.getText().toString().trim();
 
-                String name = signupName.getText().toString();
-                String email = signupEmail.getText().toString();
-                String mobile = signupMobile.getText().toString();
-                String username = signupUsername.getText().toString();
-                String password = signupPassword.getText().toString();
-                String confirmPassword = signupConfirmPassword.getText().toString();
-
-                if(name.isEmpty() || email.isEmpty() || mobile.isEmpty() || username.isEmpty() ||
-                        password.isEmpty() || confirmPassword.isEmpty()) {
-                    Toast.makeText(SignupActivity.this, "Please Enter a Valid Credentials to Register",
-                            Toast.LENGTH_SHORT).show();
-                }else {
-
+                if (validateName(name) && validateEmail(email) && validateMobile(mobile) && validateUsername(username)
+                        && validatePassword(password) && validateConfirmPassword(password, confirmPassword)) {
                     database = FirebaseDatabase.getInstance();
                     reference = database.getReference("Users");
 
@@ -63,6 +58,9 @@ public class SignupActivity extends AppCompatActivity {
                             Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
                     startActivity(intent);
+                } else {
+                    Toast.makeText(SignupActivity.this, "Please Enter Valid Credentials to Register",
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -74,5 +72,54 @@ public class SignupActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private boolean validateName(String name) {
+        if ( name.length() <= 4 || name.matches(".*\\d.*")) {
+            signupName.setError("Enter a valid name (at least 4 characters and no digits).");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateEmail(String email) {
+        if ( !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            signupEmail.setError("Enter a valid email address.");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateMobile(String mobile) {
+        if ( mobile.length() != 10 || !TextUtils.isDigitsOnly(mobile)) {
+            signupMobile.setError("Enter a valid 10-digit mobile number.");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateUsername(String username) {
+        if ( username.length() < 4 || !username.matches("^[a-zA-Z0-9_]*$")) {
+            signupUsername.setError("Enter a valid username (at least 4 characters, only letters, numbers, and underscores).");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validatePassword(String password) {
+        String passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$";
+        if ( !password.matches(passwordPattern)) {
+            signupPassword.setError("Enter a valid password (minimum 8 characters, at least one lowercase, one uppercase, one digit, and one special character).");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateConfirmPassword(String password, String confirmPassword) {
+        if (!TextUtils.equals(password, confirmPassword)) {
+            signupConfirmPassword.setError("Passwords do not match.");
+            return false;
+        }
+        return true;
     }
 }
